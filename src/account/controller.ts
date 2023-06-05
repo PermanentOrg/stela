189b538fb7f151ce49470dde8ea70a87510ecf76
Liam Lloyd <wlloyd@g.hmc.edu>
2023-06-05 14:27:36 -0700

    Fix validator util import in account controller
    The latest merge introduced an error because the path to validator utils
    had changed on `main` but not on the feature branch. This commit fixes
    that.

--- src/account/controller.ts
+++ src/account/controller.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import type { Request, Response, NextFunction } from "express";
 import { verifyUserAuthentication } from "../middleware";
 import { validateUpdateTagsRequest } from "./validators";
-import { isValidationError } from "../validator_util";
+import { isValidationError } from "../validators/validator_util";
 import { accountService } from "./service";
 
 export const accountController = Router();
