1e8fd440a46baa7e4a1868d8cf4fe7698169de46
Liam Lloyd <wlloyd@g.hmc.edu>
2023-06-13 13:21:27 -0700

    Add Mailchimp Transactional API key to terraform secrets
    Now that we're using Mailchimp Transactional in this project, terraform
    needs to know about its API key so it can supply it to the deployments.
    This commit adds the API key to terraform's secrets.

--- terraform/prod_cluster/secrets.tf
+++ terraform/prod_cluster/secrets.tf
@@ -4,8 +4,10 @@ resource "kubernetes_secret" "prod-secrets" {
   }
 
   data = {
-    "DATABASE_URL"                 = var.prod_database_url
-    "FUSIONAUTH_API_KEY"           = var.prod_fusionauth_api_key
-    "LEGACY_BACKEND_SHARED_SECRET" = var.prod_legacy_backend_shared_secret
+    "DATABASE_URL"                    = var.prod_database_url
+    "FUSIONAUTH_API_KEY"              = var.prod_fusionauth_api_key
+    "LEGACY_BACKEND_SHARED_SECRET"    = var.prod_legacy_backend_shared_secret
+    "MAILCHIMP_API_KEY"               = var.mailchimp_api_key
+    "MAILCHIMP_TRANSACTIONAL_API_KEY" = var.mailchimp_transactional_api_key
   }
 }
--- terraform/test_cluster/secrets.tf
+++ terraform/test_cluster/secrets.tf
@@ -4,10 +4,11 @@ resource "kubernetes_secret" "dev-secrets" {
   }
 
   data = {
-    "DATABASE_URL"                 = var.dev_database_url
-    "FUSIONAUTH_API_KEY"           = var.dev_fusionauth_api_key
-    "LEGACY_BACKEND_SHARED_SECRET" = var.dev_legacy_backend_shared_secret
-    "MAILCHIMP_API_KEY"            = var.mailchimp_api_key
+    "DATABASE_URL"                    = var.dev_database_url
+    "FUSIONAUTH_API_KEY"              = var.dev_fusionauth_api_key
+    "LEGACY_BACKEND_SHARED_SECRET"    = var.dev_legacy_backend_shared_secret
+    "MAILCHIMP_API_KEY"               = var.mailchimp_api_key
+    "MAILCHIMP_TRANSACTIONAL_API_KEY" = var.mailchimp_transactional_api_key
   }
 }
 
@@ -17,9 +18,10 @@ resource "kubernetes_secret" "staging-secrets" {
   }
 
   data = {
-    "DATABASE_URL"                 = var.staging_database_url
-    "FUSIONAUTH_API_KEY"           = var.staging_fusionauth_api_key
-    "LEGACY_BACKEND_SHARED_SECRET" = var.staging_legacy_backend_shared_secret
-    "MAILCHIMP_API_KEY"            = var.mailchimp_api_key
+    "DATABASE_URL"                    = var.staging_database_url
+    "FUSIONAUTH_API_KEY"              = var.staging_fusionauth_api_key
+    "LEGACY_BACKEND_SHARED_SECRET"    = var.staging_legacy_backend_shared_secret
+    "MAILCHIMP_API_KEY"               = var.mailchimp_api_key
+    "MAILCHIMP_TRANSACTIONAL_API_KEY" = var.mailchimp_transactional_api_key
   }
 }
