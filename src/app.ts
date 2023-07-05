fef57922d8fcc0d05fad3a0f595d6161e4a9f27b
Liam Lloyd <wlloyd@g.hmc.edu>
2023-07-05 16:53:52 -0700

    Add Sentry
    stela should log its errors to Sentry, like our other projects do. This
    commit adds that logging to stela's middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,29 @@
+import * as Sentry from "@sentry/node";
 import express from "express";
 import cors from "cors";
 import expressWinston from "express-winston";
 import bodyParser from "body-parser";
 import { apiRoutes } from "./routes";
 import { logger } from "./log";
-import { handleErrorWithStatus } from "./middleware/handleErrorWithStatus";
+import { handleError } from "./middleware/handleError";
 
 const env = process.env["ENV"] ?? "";
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env["SENTRY_DSN"] ?? "",
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Sentry.Integrations.Express({ app }),
+    ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  ],
+  tracesSampleRate: 1.0,
+  environment: env === "local" ? `local-${process.env["DEV_NAME"] ?? ""}` : env,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(
   cors({
     origin: `https://${env === "production" ? "www" : `${env}`}.permanent.org`,
@@ -17,6 +32,7 @@ app.use(
 app.use(expressWinston.logger({ level: "http", winstonInstance: logger }));
 app.use(bodyParser.json());
 app.use("/api/v2", apiRoutes);
-app.use(handleErrorWithStatus);
+app.use(Sentry.Handlers.errorHandler());
+app.use(handleError);
 
 export { app };
--- src/middleware/handleError.ts
+++ src/middleware/handleError.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import * as Sentry from "@sentry/node";
 
 interface ErrorWithStatus {
   status: number;
@@ -14,12 +15,13 @@ const isErrorWithStatus = (err: unknown): err is ErrorWithStatus =>
 const isErrorWithStatusCode = (err: unknown): err is ErrorWithStatusCode =>
   typeof (err as ErrorWithStatusCode).statusCode === "number";
 
-export const handleErrorWithStatus = async (
+export const handleError = async (
   err: unknown,
   _: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
+  Sentry.captureException(err);
   if (isErrorWithStatus(err)) {
     res.status(err.status).json({ error: err });
   } else if (isErrorWithStatusCode(err)) {
