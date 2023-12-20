1500de038670cd96e95754083584fea466fed1ad
Liam Lloyd <wlloyd@hmc.edu>
2023-12-19 17:48:18 -0800

    Test /health endpoint all the way through the controller
    Current tests for many parts of the stela API server don't test all the
    way through the controller. The billing controller pioneered method for
    including the controller in the tests, this commit applies that to the
    health controller.

---
+++ packages/api/src/health/controller.test.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { app } from "../app";
+import { db } from "../database";
+
+jest.mock("../database");
+
+describe("/health", () => {
+  const agent = request(app);
+
+  test("should be available when DB connection works", async () => {
+    const result = await agent.get("/api/v2/health").expect(200);
+    expect(
+      (result.body as { status: "available" | "unavailable" }).status
+    ).toBe("available");
+  });
+
+  test("should be unavailable when DB connection fails", async () => {
+    jest.spyOn(db, "sql").mockImplementation(() => {
+      throw new Error();
+    });
+
+    const result = await agent.get("/api/v2/health").expect(200);
+    expect(
+      (result.body as { status: "available" | "unavailable" }).status
+    ).toBe("unavailable");
+  });
+});
