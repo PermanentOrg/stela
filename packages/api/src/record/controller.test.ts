151d955bf8474dabcf9516a9ff4280cff0bc1978
Natalie Martin <natalie@permanent.org>
2024-02-09 11:08:08 -0700

    WIP

--- packages/api/src/record/controller.test.ts
+++ packages/api/src/record/controller.test.ts
@@ -1,5 +1,10 @@
 import request from "supertest";
 
-describe('record/get', () => {
+describe("record/get", () => {
   const agent = request(app);
-  test(
+  test("expect a non-404 response", async () => {
+    await agent.get("/api/v2/record/get").end((err, res) => {
+      expect(res.status).not.toBe(404);
+    });
+  });
+});
