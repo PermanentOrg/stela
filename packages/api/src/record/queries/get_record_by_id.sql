971026069a1d716e6d5ce6df6acda5e79e06a972
Liam Lloyd <wlloyd@hmc.edu>
2024-03-05 14:10:25 -0800

    Allow logged-in users to see other users' public records

--- packages/api/src/record/queries/get_record_by_id.sql
+++ packages/api/src/record/queries/get_record_by_id.sql
@@ -1,4 +1,4 @@
-SELECT
+SELECT DISTINCT
   recordid AS "recordId"
 FROM
   record
@@ -10,5 +10,8 @@ INNER JOIN
   ON account_archive.accountid = account.accountid
 WHERE
   recordid = ANY(:recordIds)
-  AND account.primaryemail = :accountEmail
+  AND (
+    account.primaryemail = :accountEmail
+    OR (record.publicdt IS NOT NULL AND record.publicdt <= NOW())
+  )
   AND record.status != 'status.generic.deleted';
