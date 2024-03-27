7287e65b5996dd68d5f314a6669dc25e34ef86fb
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-03-27 11:10:00 +0200

    added a beforeeach to add an email to the request

--- packages/api/src/idpuser/controller.test.ts
+++ packages/api/src/idpuser/controller.test.ts
@@ -1,11 +1,28 @@
 /** @format */
 
 import request from "supertest";
+import type { NextFunction, Request } from "express";
 import { app } from "../app";
+import { verifyUserAuthentication } from "../middleware";
 
 describe("/idpuser", () => {
   const agent = request(app);
+  beforeEach(async () => {
+    (verifyUserAuthentication as jest.Mock).mockImplementation(
+      (req: Request, __, next: NextFunction) => {
+        req.body.emailFromAuthToken = "test@permanent.org";
+        next();
+      }
+    );
+  });
   test("expect a non-404 response", async () => {
     await agent.get("/api/v2/idpuser").expect(200);
   });
+
+  // test("expect the user email from the token", async () => {
+  //   const response = await agent.get("/api/v2/idpuser");
+
+  //   expect(response.status).toBe(200);
+  //   expect(response.body.email).toEqual("test@permanent.org");
+  // });
 });
