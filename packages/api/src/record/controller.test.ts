cb280d33a7bfcb734c1a1713dbde169f52c5443c
Natalie Martin <natalie@permanent.org>
2024-04-05 11:48:09 -0500

    WIP

--- packages/api/src/record/controller.test.ts
+++ packages/api/src/record/controller.test.ts
@@ -36,13 +36,13 @@ fdescribe("record/get", () => {
   });
 
   const agent = request(app);
-  test("expect a 401 response", async () => {
+  test("expect a 200 response", async () => {
     (verifyUserAuthentication as jest.Mock).mockImplementation(
       (_, __, next: NextFunction) => {
         next(new createError.Unauthorized("You aren't logged in"));
       }
     );
-    await agent.get("/api/v2/record/get").expect(401);
+    await agent.get("/api/v2/record/get?recordIds[]=1").expect(200);
   });
   test("expect request to have an email from auth token", async () => {
     (verifyUserAuthentication as jest.Mock).mockImplementation(
