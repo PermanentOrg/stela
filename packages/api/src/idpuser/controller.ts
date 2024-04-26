d30e43c3381b9a2ce01809f9ce49cbd7fb2fc3b9
Cecilia Krum <cecilia@permanent.org>
2024-04-26 16:09:19 -0500

    Update FusionAuth call
    This is still returning a 404, so I'm not doing it quite right. The
    JWT method is deprecated, which is unfortunate, so we'll need to go
    back and remove the references to the token. Using the email instead,
    but not finding the user yet.

--- packages/api/src/idpuser/controller.ts
+++ packages/api/src/idpuser/controller.ts
@@ -8,11 +8,10 @@ import {
 } from "express";
 import fetch from "node-fetch";
 import { verifyUserAuthentication } from "../middleware";
-import { validateTokenFromBody } from "./validators";
 import { isValidationError } from "../validators/validator_util";
 import { logger } from "@stela/logger";
 
-const hostUrl = process.env["FUSIONAUTH_HOST"] ?? "";
+const fusionAuthUrl = process.env["FUSIONAUTH_HOST"] ?? "";
 
 export const idpUserController = Router();
 
@@ -21,19 +20,32 @@ idpUserController.get(
   verifyUserAuthentication,
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      logger.info(req.body)
-      validateTokenFromBody(req.body);
-
-      const response = await fetch(`${hostUrl}/api/user`, {
-        headers: {},
+      logger.info(req.body.emailFromAuthToken);
+      const fusionAuthKey = process.env["FUSIONAUTH_API_KEY"] ?? "";
+      const fusionAuthTenant = process.env["FUSIONAUTH_TENANT"] ?? "";
+      const userUrl = fusionAuthUrl + '/api/user?email=' + req.body.emailFromAuthToken;
+      logger.info(userUrl);
+      // needs API key in auth header. JWT is deprecated!
+      const response = await fetch(userUrl, {
+        headers: {
+          "Authentication": fusionAuthKey,
+          "X-FusionAuth-TenantId": fusionAuthTenant,
+        },
       });
-      res.send([response]);
-    } catch (error) {
-      if (isValidationError(error)) {
-        res.status(400).json({ error });
+      if (response.status == 200) {
+        const userInfo = await response.json();
+        logger.info(userInfo);
+        res.send([userInfo]);
+      } else {
+        logger.info(response.status);
+        res.send(response.status);
+      }
+    } catch (err) {
+      if (isValidationError(err)) {
+        res.status(400).json({ error: err });
         return;
       }
-      next(error);
+      next(err);
     }
   }
 );
