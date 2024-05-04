024f5bdef61661d844d8c122c04f48edd31e8c1b
Liam Lloyd <wlloyd@hmc.edu>
2024-05-03 17:23:21 -0700

    Add script to clean up the Archivematica dashboard
    Once we start processing files with Archivematica, we will need to
    periodically clean out the Archivematica dashboard, to prevent it from
    being unusably slow if we ever log in to debug something or update
    configuration. This commit adds a script that cleans out all completed
    jobs from the dashboard. A future commit will set up this script to run
    on a Kubernetes CronJob workload.

---
+++ packages/archivematica_cleanup/src/index.test.ts
@@ -0,0 +1,83 @@
+import * as Sentry from "@sentry/node";
+import { cleanupDashboard } from "./index";
+
+jest.mock("@sentry/node");
+
+describe("cleanupDashboard", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+  test("should call fetch with the correct arguments", async () => {
+    global.fetch = jest.fn(async () =>
+      Promise.resolve({
+        ok: true,
+      } as Response)
+    ) as jest.Mock;
+
+    await cleanupDashboard();
+    expect(global.fetch).toHaveBeenNthCalledWith(
+      1,
+      `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/transfer/delete`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
+        },
+      }
+    );
+    expect(global.fetch).toHaveBeenNthCalledWith(
+      2,
+      `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/ingest/delete`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
+        },
+      }
+    );
+  });
+
+  test("should log an error to sentry if the transfer deletion fails", async () => {
+    const errorText = "500 Internal Server Error";
+    global.fetch = jest.fn(async () =>
+      Promise.resolve({
+        ok: false,
+        text: async () => Promise.resolve(errorText),
+      } as Response)
+    ) as jest.Mock;
+
+    await cleanupDashboard();
+    expect(Sentry.captureMessage).toHaveBeenCalledWith(errorText);
+  });
+
+  test("should log an error to sentry if the ingest deletion fails", async () => {
+    const errorText = "500 Internal Server Error";
+    global.fetch = jest
+      .fn()
+      .mockImplementationOnce(async () =>
+        Promise.resolve({
+          ok: true,
+        } as Response)
+      )
+      .mockImplementationOnce(async () =>
+        Promise.resolve({
+          ok: false,
+          text: async () => Promise.resolve(errorText),
+        } as Response)
+      );
+
+    await cleanupDashboard();
+    expect(Sentry.captureMessage).toHaveBeenCalledWith(errorText);
+  });
+
+  test("should log an error to sentry if an exception is thrown", async () => {
+    const error = new Error("Out of cheese - Redo from start");
+    global.fetch = jest
+      .fn()
+      .mockImplementationOnce(async () => Promise.reject(error));
+
+    await cleanupDashboard();
+    expect(Sentry.captureException).toHaveBeenCalledWith(error);
+  });
+});
