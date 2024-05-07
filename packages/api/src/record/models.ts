102fd439fefc39e20a0a177c03ae65d40840c79e
Cecilia Krum <cecilia@permanent.org>
2024-05-07 16:17:07 -0500

    Manage the type of the file size

--- packages/api/src/record/models.ts
+++ packages/api/src/record/models.ts
@@ -14,3 +14,20 @@ export interface ArchiveRecord {
   fileCreatedAt?: string;
   imageRatio?: number;
 }
+
+export interface ArchiveRecordRow {
+  recordId: string;
+  archiveId: string;
+  displayName: string;
+  archiveNumber?: string;
+  description?: string;
+  publicAt?: string;
+  downloadName?: string;
+  uploadFileName: string;
+  uploadAccountId: string;
+  uploadPayerAccountId?: string;
+  size?: string;
+  displayDate?: string;
+  fileCreatedAt?: string;
+  imageRatio?: number;  
+}
