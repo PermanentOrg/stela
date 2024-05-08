2c5f352ae80981d14e7a47adbabd419b8737974c
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-05-08 16:22:00 +0300

    Code changes to pass linting

--- packages/api/src/idpuser/controller.ts
+++ packages/api/src/idpuser/controller.ts
@@ -10,6 +10,7 @@ import { fusionAuthClient } from "../fusionauth";
 import { verifyUserAuthentication } from "../middleware";
 import { isValidationError } from "../validators/validator_util";
 import { validateBodyFromAuthentication } from "../validators";
+import type { TwoFactorRequestResponse } from "./models";
 
 export const idpUserController = Router();
 
@@ -19,18 +20,18 @@ idpUserController.get(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       validateBodyFromAuthentication(req.body);
-      let response = [];
+      let response: TwoFactorRequestResponse[] = [];
       const clientResponse = await fusionAuthClient.retrieveUserByEmail(
         req.body.emailFromAuthToken
       );
-      response = clientResponse.response.user?.twoFactor?.methods ?? [];
-
-      if (response.length) {
-        response = response.map(({ id, method, email, mobilePhone }) => ({
-          methodId: id,
-          method,
-          value: email ?? mobilePhone,
-        }));
+      if (clientResponse.response.user.twoFactor.methods.length) {
+        response = clientResponse.response.user.twoFactor.methods.map(
+          ({ id, method, email, mobilePhone }) => ({
+            methodId: id,
+            method,
+            value: email || mobilePhone,
+          })
+        );
       }
 
       res.send(response);
