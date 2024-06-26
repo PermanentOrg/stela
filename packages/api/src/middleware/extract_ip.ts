bf83724e5011daea22379a1eb0d127d9aec43777
Natalie Martin <natalie@permanent.org>
2024-06-26 14:32:10 -0700

    Parse list of IPs properly in `extractIp`
    The back-end will pass a list of proxies from the X-Forwarded-For
    header, if needed[1]. The `extractIp` middleware was not written
    to expect this format, and so would cause validation errors when passed
    a list of proxy IPs. Update this middleware function to handle a proxy
    IP list by extracting out the client IP from the list.
    
    [1]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For#syntax
    
    PER-9651: "Publish archive" does not get checked off

--- packages/api/src/middleware/extract_ip.ts
+++ packages/api/src/middleware/extract_ip.ts
@@ -1,10 +1,23 @@
 import type { Request, Response, NextFunction } from "express";
 
+const extractClientIpFromProxyList = (
+  list: string | undefined
+): string | undefined => {
+  if (typeof list !== "undefined") {
+    if (list.includes(",")) {
+      return list.split(",").shift()?.trim();
+    }
+  }
+  return list;
+};
+
 export const extractIp = (
   req: Request<unknown, unknown, Record<string, string | undefined>>,
   _: Response,
   next: NextFunction
 ): void => {
-  req.body["ip"] = req.get("x-forwarded-for") ?? req.connection.remoteAddress;
+  req.body["ip"] =
+    extractClientIpFromProxyList(req.get("X-Forwarded-For")) ??
+    req.connection.remoteAddress;
   next();
 };
