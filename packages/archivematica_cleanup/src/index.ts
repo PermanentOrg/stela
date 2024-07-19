a543093c4581ee830f1eca385788b32db3d8e3ea
Liam Lloyd <wlloyd@hmc.edu>
2024-07-18 17:10:01 -0700

    Fix Sentry logging from Archivematica cleanup cron
    The Archivematica cleanup cron is supposed to send error logs to Sentry,
    but it currently doesn't. This seems to be because we needed to include
    an extra Sentry package; this commit adds it.

--- packages/archivematica_cleanup/src/index.ts
+++ packages/archivematica_cleanup/src/index.ts
@@ -1,47 +1,44 @@
 import * as Sentry from "@sentry/node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import { logger } from "@stela/logger";
 import "./env";
 
 const env = process.env["ENV"] ?? "";
 Sentry.init({
   dsn: process.env["SENTRY_DSN"] ?? "",
+  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
   environment: env === "local" ? `local-${process.env["DEV_NAME"] ?? ""}` : env,
 });
 
 export const cleanupDashboard = async (): Promise<void> => {
-  try {
-    const transferDeleteResponse = await fetch(
-      `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/transfer/delete`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
-        },
-      }
-    );
-    if (!transferDeleteResponse.ok) {
-      logger.error(await transferDeleteResponse.text());
-      Sentry.captureMessage(await transferDeleteResponse.text());
-      return;
+  const transferDeleteResponse = await fetch(
+    `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/transfer/delete`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
+      },
     }
-    const ingestDeleteResponse = await fetch(
-      `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/ingest/delete`,
-      {
-        method: "DELETE",
-        headers: {
-          Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
-        },
-      }
-    );
-    if (!ingestDeleteResponse.ok) {
-      logger.error(await ingestDeleteResponse.text());
-      Sentry.captureMessage(await ingestDeleteResponse.text());
-      return;
+  );
+  if (!transferDeleteResponse.ok) {
+    logger.error(await transferDeleteResponse.text());
+    Sentry.captureMessage(await transferDeleteResponse.text());
+    return;
+  }
+  const ingestDeleteResponse = await fetch(
+    `${process.env["ARCHIVEMATICA_BASE_URL"] ?? ""}/api/ingest/delete`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `ApiKey ${process.env["ARCHIVEMATICA_API_KEY"] ?? ""}`,
+      },
     }
-  } catch (error) {
-    logger.error(error);
-    Sentry.captureException(error);
+  );
+  if (!ingestDeleteResponse.ok) {
+    logger.error(await ingestDeleteResponse.text());
+    Sentry.captureMessage(await ingestDeleteResponse.text());
   }
 };
 
@@ -50,6 +47,7 @@ cleanupDashboard()
     process.exit(0);
   })
   .catch((error) => {
+    logger.error(error);
     Sentry.captureException(error);
     process.exit(1);
   });
