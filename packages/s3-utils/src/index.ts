76af05c9aae14390e7be0c5a7d651275184828a3
Liam Lloyd <wlloyd@hmc.edu>
2024-11-05 15:00:41 -0800

    Extract function to parse S3 event messages to s3-utils
    Both the record thumbnail lambda and the access copy lambda need to
    parse notifications from S3. To avoid duplicated code, this commit adds
    a function that does that to the s3-utils package.

--- packages/s3-utils/src/index.ts
+++ packages/s3-utils/src/index.ts
@@ -1,4 +1,11 @@
 import { getSignedUrl } from "aws-cloudfront-sign";
+import type { SQSRecord } from "aws-lambda";
+import { logger } from "@stela/logger";
+import {
+  validateNewDisseminationPackageJpgEvent,
+  validateSqsMessage,
+  type S3Object,
+} from "./validators";
 
 const yearsUntilCdnUrlExpiration = 1;
 
@@ -7,7 +14,7 @@ export const constructSignedCdnUrl = (
   fileName?: string
 ): string => {
   let url = `${process.env["CLOUDFRONT_URL"] ?? ""}${key}`;
-  if (fileName) {
+  if (fileName !== undefined) {
     const urlObject = new URL(url);
     urlObject.searchParams.append("response-content-disposition", fileName);
     url = urlObject.toString();
@@ -22,4 +29,29 @@ export const constructSignedCdnUrl = (
     keypairId: process.env["CLOUDFRONT_KEY_PAIR_ID"] ?? "",
     privateKeyString: process.env["CLOUDFRONT_PRIVATE_KEY"] ?? "",
   });
+};
+
+export const getS3ObjectFromS3Message = (message: SQSRecord): S3Object => {
+  const { body } = message;
+  const parsedBody: unknown = JSON.parse(body);
+  if (!validateSqsMessage(parsedBody)) {
+    logger.error(
+      `Invalid message body: ${JSON.stringify(validateSqsMessage.errors)}`
+    );
+    throw new Error("Invalid message body");
+  }
+  const parsedMessage: unknown = JSON.parse(parsedBody.Message);
+  if (
+    !validateNewDisseminationPackageJpgEvent(parsedMessage) ||
+    !parsedMessage.Records[0]
+  ) {
+    logger.error(
+      `Invalid message body: ${JSON.stringify(
+        validateNewDisseminationPackageJpgEvent.errors
+      )}`
+    );
+    throw new Error("Invalid message body");
+  }
+
+  return parsedMessage.Records[0].s3.object;
 };
---
+++ packages/s3-utils/src/validators.ts
@@ -0,0 +1,72 @@
+import Ajv from "ajv";
+import type { JSONSchemaType } from "ajv";
+
+const ajv = new Ajv({ coerceTypes: true });
+
+export interface SQSMessage {
+  Message: string;
+}
+
+const sqsMessageSchema: JSONSchemaType<SQSMessage> = {
+  type: "object",
+  properties: {
+    Message: { type: "string" },
+  },
+  required: ["Message"],
+  additionalProperties: true,
+};
+
+export const validateSqsMessage = ajv.compile(sqsMessageSchema);
+
+export interface S3Object {
+  key: string;
+  size: number;
+  versionId: string;
+}
+
+export interface NewDisseminationPackageJpgEvent {
+  Records: {
+    s3: {
+      object: S3Object;
+    };
+  }[];
+}
+const newDisseminationPackageJpgEventSchema: JSONSchemaType<NewDisseminationPackageJpgEvent> =
+  {
+    type: "object",
+    properties: {
+      Records: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            s3: {
+              type: "object",
+              properties: {
+                object: {
+                  type: "object",
+                  properties: {
+                    key: { type: "string" },
+                    size: { type: "number" },
+                    versionId: { type: "string" },
+                  },
+                  required: ["key"],
+                  additionalProperties: true,
+                },
+              },
+              required: ["object"],
+              additionalProperties: true,
+            },
+          },
+          required: ["s3"],
+          additionalProperties: true,
+        },
+      },
+    },
+    required: ["Records"],
+    additionalProperties: true,
+  };
+
+export const validateNewDisseminationPackageJpgEvent = ajv.compile(
+  newDisseminationPackageJpgEventSchema
+);
