2b385d8169a28e22202b99ea4a4774322d2bb21a
Liam Lloyd <wlloyd@hmc.edu>
2024-11-05 16:07:27 -0800

    Update record thumbnail lambda to use function from archivematica-utils
    This commit updates the record thumbnail lambda to use a function from
    archivematica-utils to extract fileIds from S3 paths to Archivematica
    packages, rather than its own copy of that function (which is deleted).

--- packages/record_thumbnail_attacher/src/index.ts
+++ packages/record_thumbnail_attacher/src/index.ts
@@ -4,20 +4,9 @@ import {
   constructSignedCdnUrl,
   getS3ObjectFromS3Message,
 } from "@stela/s3-utils";
+import { getOriginalFileIdFromInformationPackagePath } from "@stela/archivematica-utils";
 import { logger } from "@stela/logger";
 import { db } from "./database";
-
-const getFileIdFromKey = (key: string): string => {
-  // The key will inlude the substring /{fileId}_upload, which is what this regex looks for. The fileId is either
-  // numeric or a UUID, so we expect it to consist of some hexadecimal digits and hyphens.
-  const fileIdRegex = /\/([0-9abcdef-]*)_upload/;
-  const match = fileIdRegex.exec(key);
-  if (match === null || match[1] === undefined) {
-    logger.error(`Invalid file key: ${key}`);
-    throw new Error("Invalid file key");
-  }
-  return match[1];
-};
 
 export const handler: SQSHandler = Sentry.wrapHandler(
   async (event: SQSEvent, _, __) => {
@@ -29,7 +18,7 @@ export const handler: SQSHandler = Sentry.wrapHandler(
           return;
         }
 
-        const fileId = getFileIdFromKey(key);
+        const fileId = getOriginalFileIdFromInformationPackagePath(key);
         const thumbnailUrl = constructSignedCdnUrl(key);
 
         try {
