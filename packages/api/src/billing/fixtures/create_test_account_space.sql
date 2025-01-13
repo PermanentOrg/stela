c44eb02534187f4d67eb9e5a379ade5d05ae52b3
iuliandanea <iuliandanea@vspartners.us>
2025-01-13 13:36:41 +0200

    PER-9598 Break up test fixtures in stela by controller - billing

---
+++ packages/api/src/billing/fixtures/create_test_account_space.sql
@@ -0,0 +1,46 @@
+INSERT INTO
+account_space (
+  accountid,
+  spaceleft,
+  spacetotal,
+  fileleft,
+  filetotal,
+  status,
+  type,
+  createddt,
+  updateddt
+)
+VALUES
+(
+  2,
+  2147483648,
+  2147483648,
+  100000,
+  100000,
+  'status.generic.ok',
+  'type.generic.placeholder',
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP
+),
+(
+  3,
+  2147483648,
+  2147483648,
+  100000,
+  100000,
+  'status.generic.ok',
+  'type.generic.placeholder',
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP
+),
+(
+  4,
+  2147483648,
+  2147483648,
+  100000,
+  100000,
+  'status.generic.ok',
+  'type.generic.placeholder',
+  CURRENT_TIMESTAMP,
+  CURRENT_TIMESTAMP
+);
