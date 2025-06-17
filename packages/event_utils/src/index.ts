ab0caf0dd4008ff640ebcbf35726ffd5bc7a1ece
Liam Lloyd-Tucker <wlloyd@hmc.edu>
2025-06-17 12:41:08 -0700

    Move event validators to new event_utils package
    The new archivematica_triggerer lambda will need the same event
    validator functions as the account_space_updater, so this commit moves
    them to a shared package.

---
+++ packages/event_utils/src/index.ts
@@ -0,0 +1,53 @@
+import Ajv from "ajv";
+import type { JSONSchemaType } from "ajv";
+
+const ajv = new Ajv({ coerceTypes: true });
+
+interface RecordSubmitEvent {
+	entity: string;
+	action: string;
+	body: {
+		record?: {
+			recordId: string;
+		};
+		newRecord?: {
+			recordId: string;
+		};
+	};
+}
+
+const recordSubmitEventSchema: JSONSchemaType<RecordSubmitEvent> = {
+	type: "object",
+	properties: {
+		entity: { type: "string" },
+		action: { type: "string" },
+		body: {
+			type: "object",
+			properties: {
+				record: {
+					type: "object",
+					properties: {
+						recordId: { type: "string" },
+					},
+					required: ["recordId"],
+					additionalProperties: true,
+					nullable: true,
+				},
+				newRecord: {
+					type: "object",
+					properties: {
+						recordId: { type: "string" },
+					},
+					required: ["recordId"],
+					additionalProperties: true,
+					nullable: true,
+				},
+			},
+			additionalProperties: true,
+		},
+	},
+	required: ["body", "entity", "action"],
+	additionalProperties: true,
+};
+
+export const validateRecordSubmitEvent = ajv.compile(recordSubmitEventSchema);
