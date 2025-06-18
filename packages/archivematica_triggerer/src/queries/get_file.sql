b998902802eeb6819608b3072e5e427facc89cba
Liam Lloyd-Tucker <wlloyd@hmc.edu>
2025-06-18 12:59:50 -0700

    Add archivematica triggerer lambda
    To avoid cases where synchronous endpoints are delayed by Archivematica
    timeouts, and to enable retries when we fail to launch Archivematica
    processing, we want to trigger archivematica processing asynchronously.
    This commit adds a lambda that does so when a record.create event
    occurs.

---
+++ packages/archivematica_triggerer/src/queries/get_file.sql
@@ -0,0 +1,13 @@
+SELECT
+	file.fileid AS "fileId",
+	file.cloudpath AS "filePath"
+FROM
+	file
+INNER JOIN
+	record_file
+ON
+	file.fileid = record_file.fileid
+WHERE
+	record_file.recordid = :recordId
+	AND file.format = 'file.format.original'
+	AND file.status != 'status.generic.deleted';
