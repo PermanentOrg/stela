b8cf61c5205e40b4ba426eb969fd0a182c416e23
Liam Lloyd-Tucker <wlloyd@hmc.edu>
2025-07-10 15:13:57 -0700

    Enable @typescript-eslint/consistent-type-assertions linting rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -53,8 +53,6 @@ export default defineConfig([
 
 			// These are `love` rules that we were violating.  We should review these one at a time.
 			"@typescript-eslint/no-magic-numbers": "off",
-			"@typescript-eslint/consistent-type-assertions": "off",
-			"@typescript-eslint/consistent-type-assertions": "off",
 			"@typescript-eslint/no-unnecessary-type-conversion": "off",
 			complexity: "off",
 			"@typescript-eslint/no-unnecessary-condition": "off",
