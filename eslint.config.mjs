873bbb1c556c312f1408e66a8654eba73d65f6d0
Liam Lloyd-Tucker <wlloyd@hmc.edu>
2025-07-11 16:03:38 -0700

    Enable import/no-extraneous-dependencies linting rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,7 +31,6 @@ export default defineConfig([
 		rules: {
 			"import/prefer-default-export": "off",
 			"import/no-default-export": "error",
-			"import/no-extraneous-dependencies": "off",
 			"@typescript-eslint/require-await": "off",
 			"@typescript-eslint/prefer-readonly-parameter-types": "off",
 
