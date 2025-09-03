621b13e899530148396ac4b5f83e2d8231bd0b39
Liam Lloyd-Tucker <liam@permanent.org>
2025-09-03 15:37:38 -0700

    Fix middleware that fails on undefined requests
    Before switching to the express.json() middleware, empty requests bodies
    were represented by empty objects, meaning middleware that added things
    to the request body was always able to do so. Now, however, empty
    request bodies are undefined, meaning such middleware fails. This commit
    adds an extra piece of middleware to run after express.json() and set
    request.body to {} if it is undefined, allowing the rest of the
    middleware to run successfully. Note that this is a stopgap, we should
    eventually update all the middleware not to modify the request body.

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -19,6 +19,16 @@ app.use(
 );
 app.use(expressWinston.logger({ level: "http", winstonInstance: logger }));
 app.use(express.json());
+
+// This is a temporary measure; it should be removed when the rest of
+// our middleware is updated not to add request metadata to the request body
+app.use((req, _res, next) => {
+	if (req.body === undefined) {
+		req.body = {};
+	}
+	next();
+});
+
 app.use("/api/v2", apiRoutes);
 Sentry.setupExpressErrorHandler(app);
 app.use(handleError);
